54b3739cb1fdb13bfe14ea2eab6930ca1b449a8f
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2024-06-21 14:01:22 +0300

    [150] [Time Beats: 53.39%] [Memory Beats: 72.08%] - LeetPush

---
+++ 150-Evaluate-Reverse-Polish-Notation.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for token in tokens:
+            if token in "+-*/":
+                b = int(stack.pop())
+                a = int(stack.pop())
+                if token == "+":
+                    stack.append(a + b)
+                elif token == "-":
+                    stack.append(a - b)
+                elif token == "*":
+                    stack.append(a * b)
+                elif token == "/":
+                    stack.append(int(float(a) / b))
+            else:
+                stack.append(int(token))
+        return stack[0]
