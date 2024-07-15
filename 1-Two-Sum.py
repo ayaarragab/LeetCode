4296b0576c54a9f82c9f84f406eef690f4668a21
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2024-07-15 14:30:21 +0300

    [1] [Time Beats: 49.87%] [Memory Beats: 87.73%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -5,21 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        num_to_index = {}
-
         for i, num in enumerate(nums):
-            complement = target - num
-
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-
-            num_to_index[num] = i
-
-        if target == 0:
-            if nums.count(0) == 2:
-                idx = nums.index(0)
-                return [idx, nums.index(0, idx + 1)]
-            else:
-                for num in nums:
-                    if -num in nums:
-                        return [nums.index(num), nums.index(-num)]   
+            if (target - num in nums and nums.index(target - num) != i):
+                idx = nums.index(target - num)
+                return [i, idx]
+        
