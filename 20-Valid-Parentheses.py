a6a6c5e3399a8c7140a550d2a35ce20b637889e6
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2024-07-26 23:15:28 +0300

    [20] [Time Beats: 20.81%] [Memory Beats: 76.32%] - LeetPush

--- 20-Valid-Parentheses.py
+++ 20-Valid-Parentheses.py
@@ -1,22 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        """
+        \\\
         :type s: str
         :rtype: bool
-        """
-        if len(s) == 1 or s[0] in "})]":
-            return False
+        \\\
         stack = []
-        hashMap = {"]":"[", "}":"{", ")":"("}
-        for i, l in enumerate(s):
-            if l in "{([":
-                stack.append(l)
-                continue
-            else:
-                t = hashMap[l]
-                if stack and stack[-1] == t:
+        m = {'}':'{', ']':'[', ')':'('}
+        for c in s:
+            if stack and c in m:
+                if stack[-1] == m[c]:
                     stack.pop()
                 else:
                     return False
-        if stack == []:
-            return True
+            else:
+                stack.append(c)
+        return not stack
