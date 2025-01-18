bcd17fb4d7ffeffe9be981f1c14409a1b3f672e5
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2025-01-18 09:53:05 +0200

    [21] [Time Beats: 100.00%] [Memory Beats: 26.53%] - LeetPush

--- 21-Merge-Two-Sorted-Lists.py
+++ 21-Merge-Two-Sorted-Lists.py
@@ -5,74 +5,36 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        \\\
+        """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
-        \\\
+        """
         if not list1:
             return list2
         if not list2:
             return list1
-        head = list1
-        last_in_list1 = head
-        while last_in_list1.next:
-            last_in_list1 = last_in_list1.next
-        last_in_list1.next = list2
-        if not head.next.next:
-            if head.val > head.next.val:
-                temp = head
-                head = head.next
-                head.next = temp
-                temp.next = None
-                return head
-            elif head.val <= head.next.val:
-                return head
-        back = None
-        swap = True
-        fixed = head
-        prev = fixed
-        _next = prev.next
-        while fixed and swap:
-            swap = False
-            back = None
-            fixed = head
-            prev = fixed
-            _next = prev.next
-            swaps = []
-            while _next:
-                if prev.val > _next.val:
-                    save = _next.next
-                    if not back:
-                        first_node = True
-                        prev.next = save
-                        _next.next = prev
-                        back = _next
-                        head = back
-                        if prev == fixed:
-                            fixed = _next
-                    else:
-                        first_node = False
-                        back.next = _next
-                        _next.next = prev
-                        prev.next = save
-                    swaps.append(True)
-                    temp = _next
-                    _next = prev
-                    prev = temp
+        node = None
+        head = node
+        while list1 and list2:
+            if list1.val < list2.val:
+                if not node:
+                    node = list1
+                    head = node
                 else:
-                    if not back:
-                        back = head
-                        first_node = True
-                    else:
-                        first_node = False
-                    swaps.append(False)
-                if not _next.next:
-                    if any(swaps):
-                        swap = True
-                    else:
-                        swap = False
-                back = back.next if not first_node else back
-                prev = prev.next
-                _next = _next.next if _next else None
+                    node.next = list1
+                    node = node.next
+                list1 = list1.next
+            else:
+                if not node:
+                    node = list2
+                    head = node
+                else:
+                    node.next = list2
+                    node = node.next
+                list2 = list2.next
+        if list1:
+            node.next = list1
+        elif list2:
+            node.next = list2
         return head
