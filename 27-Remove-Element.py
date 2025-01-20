e732e548621a30d240c1cd07daf436add24f190c
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2025-01-20 20:52:16 +0200

    [27] [Time Beats: 100.00%] [Memory Beats: 5.64%] - LeetPush

--- 27-Remove-Element.py
+++ 27-Remove-Element.py
@@ -5,10 +5,19 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        l = len(nums)
-        k = 0
-        for i in range(l):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-        return k
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            if nums[0] == val:
+                return 0
+            else:
+                return 1
+        numOcc = 0
+        for num in nums:
+            if num == val:
+                numOcc += 1
+        for i in range(numOcc):
+            for i, num in enumerate(nums):
+                if num == val:
+                    del nums[i]
+        return len(nums)
