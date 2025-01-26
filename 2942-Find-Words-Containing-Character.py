48af1f8a234ae812a55e343e6788b28b70cb4b5a
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2025-01-26 22:26:24 +0200

    [2942] [Time Beats: 100.00%] [Memory Beats: 39.40%] - LeetPush

--- 2942-Find-Words-Containing-Character.py
+++ 2942-Find-Words-Containing-Character.py
@@ -5,8 +5,8 @@ class Solution(object):
         :type x: str
         :rtype: List[int]
         """
-        arr = []
-        for i, w in enumerate(words):
-            if x in w:
-                arr.append(i)
-        return arr
+        ans = []
+        for i in range(len(words)):
+            if x in words[i]:
+                ans.append(i)
+        return ans
