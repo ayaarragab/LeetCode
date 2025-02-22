92045e997d58b1938324dddc8ed36221712ce2d4
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2025-02-22 08:58:04 +0200

    [39] [Time Beats: 31.44%] [Memory Beats: 87.65%] - LeetPush

--- 39-Combination-Sum.py
+++ 39-Combination-Sum.py
@@ -5,17 +5,13 @@ class Solution:
 
         def dfs(idx, current_sum):
             if current_sum == target:
-                combinations.append(arrangement.copy())
+                combinations.append(arrangement.copy()) # .copy()
                 return
-            if current_sum > target or idx >= len(candidates):
-                return  #
-
+            elif current_sum > target or idx >= len(candidates):
+                return
             arrangement.append(candidates[idx])
             dfs(idx, current_sum + candidates[idx])
-
             arrangement.pop()
-
             dfs(idx + 1, current_sum)
-
         dfs(0, 0)
         return combinations
