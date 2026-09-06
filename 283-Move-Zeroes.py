8ce4adc1628ef5fb04c701159a37f4a0f86c94d5
Aya Ragab <ayarsk4343@gmail.com>
2026-09-06 09:21:49 +0300

    [283] [Time Beats: 9.46%] [Memory Beats: 50.27%] - LeetPush

--- 283-Move-Zeroes.py
+++ 283-Move-Zeroes.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        \\\
+        """
         :type nums: List[int]
         :rtype: None. Do not return anything, modify nums in-place instead.
-        \\\
+        """
         insert_pos = 0
         for i in range(len(nums)):
             if nums[i] != 0:
