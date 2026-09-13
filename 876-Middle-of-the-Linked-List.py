8bf8824128c6f664c9c9305eb5cf708a0264e653
Aya Ragab <ayarsk4343@gmail.com>
2026-09-13 14:56:01 +0300

    [876] [Time Beats: 100.00%] [Memory Beats: 91.72%] - LeetPush

--- 876-Middle-of-the-Linked-List.py
+++ 876-Middle-of-the-Linked-List.py
@@ -1,4 +1,3 @@
-import math
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
@@ -10,25 +9,13 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if not head:
-            return None
-        if not head.next:
+        if not head or not head.next:
             return head
-        if not head.next.next:
-            return head.next
-        length = 0
-        curr = head
-        while curr:
-            curr = curr.next
-            length += 1
-        
-        if length % 2 == 0:
-            length = length // 2
-        else:
-            length = math.ceil(length / 2)
-
-        curr2 = head
-        while curr2 and length > 0:
-            curr2 = curr2.next
-            length -= 1
-        return curr2
+        slow, fast = head, head
+        while fast:
+            if fast.next:
+                fast = fast.next.next
+            else:
+                break
+            slow = slow.next
+        return slow
